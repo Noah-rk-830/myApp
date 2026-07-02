43c1ed8495d1733e705192eec322d6d87d3db4a4
tanii <tanii@neskk.co.jp>
2026-07-02 11:19:07 +0900

    編集

--- static/js/script.js
+++ static/js/script.js
@@ -24,3 +24,86 @@ window.addEventListener('pageshow', function (event) {
         initPage();
     }
 });
+
+const menuItems = document.querySelectorAll("#menuList li");
+
+// 現在選択中のメニュー番号
+let selectedIndex = 0;
+
+// 選択表示更新
+function updateSelection() {
+
+    menuItems.forEach(item => {
+        item.classList.remove("selected");
+    });
+
+    menuItems[selectedIndex].classList.add("selected");
+
+}
+
+// メニュー決定処理
+function executeMenu() {
+
+    const action = menuItems[selectedIndex].dataset.action;
+
+    switch (action) {
+        default:
+            alert("メニューが指定されていません。");
+    }
+}
+
+// キーボード操作
+document.addEventListener("keydown", function (e) {
+
+    switch (e.key) {
+        case "ArrowUp":
+            selectedIndex--;
+            if (selectedIndex < 0) {
+                selectedIndex = menuItems.length - 1;
+            }
+            updateSelection();
+            e.preventDefault();
+            break;
+
+        case "ArrowDown":
+            selectedIndex++;
+            if (selectedIndex >= menuItems.length) {
+                selectedIndex = 0;
+            }
+            updateSelection();
+            e.preventDefault();
+            break;
+
+        case "Enter":
+            executeMenu();
+            e.preventDefault();
+            break;
+    }
+
+});
+
+// マウス操作
+menuItems.forEach((item, index) => {
+
+    // マウスを乗せたら選択
+    item.addEventListener("mouseenter", function () {
+
+        selectedIndex = index;
+        updateSelection();
+
+    });
+
+    // クリックで決定
+    item.addEventListener("click", function () {
+
+        selectedIndex = index;
+        updateSelection();
+
+        executeMenu();
+
+    });
+
+});
+
+// 初期表示
+updateSelection();
--- static/js/title.js
+++ static/js/title.js
@@ -1,23 +1,6 @@
 /****************************************
     タイトルメニュー
 ****************************************/
-
-const menuItems = document.querySelectorAll("#menuList li");
-
-// 現在選択中のメニュー番号
-let selectedIndex = 0;
-
-// 選択表示更新
-function updateSelection() {
-
-    menuItems.forEach(item => {
-        item.classList.remove("selected");
-    });
-
-    menuItems[selectedIndex].classList.add("selected");
-
-}
-
 // メニュー決定処理
 function executeMenu() {
 
@@ -37,59 +20,3 @@ function executeMenu() {
             break;
     }
 }
-
-// キーボード操作
-document.addEventListener("keydown", function (e) {
-
-    switch (e.key) {
-        case "ArrowUp":
-            selectedIndex--;
-            if (selectedIndex < 0) {
-                selectedIndex = menuItems.length - 1;
-            }
-            updateSelection();
-            e.preventDefault();
-            break;
-
-        case "ArrowDown":
-            selectedIndex++;
-            if (selectedIndex >= menuItems.length) {
-                selectedIndex = 0;
-            }
-            updateSelection();
-            e.preventDefault();
-            break;
-
-        case "Enter":
-            executeMenu();
-            e.preventDefault();
-            break;
-    }
-
-});
-
-// マウス操作
-menuItems.forEach((item, index) => {
-
-    // マウスを乗せたら選択
-    item.addEventListener("mouseenter", function () {
-
-        selectedIndex = index;
-        updateSelection();
-
-    });
-
-    // クリックで決定
-    item.addEventListener("click", function () {
-
-        selectedIndex = index;
-        updateSelection();
-
-        executeMenu();
-
-    });
-
-});
-
-// 初期表示
-updateSelection();
